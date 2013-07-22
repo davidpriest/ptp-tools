0c048b42856a3106f932e4d6c582c0abd1e58ea4
David Priest <david@davidpriest.ca>
2013-07-21 17:54:29 -0700

    Update ptp scripts

--- ptp/publish_diff.sh
+++ ptp/publish_diff.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
+# The following values are set in the source directory's publish.bat stub
+# DOCUMENT=_The_Root_Document.txt
+# PTP_TOOLS=$PWD/..
+
+# Do not modify below this line
+
+DOCUMENT=$1
+shift
+EXTRA=$*
 
 [ -z "$PTP_TOOLS" ] && source ../init.sh
+
+mkdir -p output
+rm -rf output/*
+
+## TODO: put this in quine
 
 java -cp $PTP_TOOLS/diffmk/bin/diffmk.jar net.sf.diffmk.DiffMk --ignorewhitespace "$1" "$2" "output/DIFF.xml"
 
